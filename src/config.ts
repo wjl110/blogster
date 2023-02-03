54bc37940a80a460df284c90038aba9058677811
wjl110 <184313947@qq.com>
2023-02-03 15:02:15 +0800

    init commit

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Kindle阅读、考研、笔记、心得";
 export const SITE_DESCRIPTION =
   "Welcome to my blog! I write about modern web development.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
